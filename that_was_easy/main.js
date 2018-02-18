1bac4112cb7acc89c4e5796dfdaf9038c3f4471a
Vincent Barletta <vb3472@pleasantonusd.net>
2018-02-18 00:33:04 +0000

    Add key binding to that was easy button

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -1,5 +1,14 @@
-functon sayThatWasEasy() {
-var thatWasEasy = new Audio("that_was_easy (1).mp3")
-thatWasEasy.play();
+function sayThatWasEasy() {
+  var thatWasEasy = new Audio("that_was_easy.mp3");
+  thatWasEasy.play();
 }
-$("#easy").on("click",sayThatWasEasy);
+
+$("#easy").on("click", sayThatWasEasy);
+
+$(document).keypress(delegateKeypress);
+
+function delegateKeypress(event) {
+  if (event.charCode == 32) {
+    $("#easy").trigger("click");
+  }
+}
