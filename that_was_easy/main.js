f7d2a4628d6e26e852a95b5fbc7a54e9dc4cd268
Vincent Barletta <vb3472@pleasantonusd.net>
2018-02-18 02:54:12 +0000

    Create that was easy button

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -1,12 +1,10 @@
 function sayThatWasEasy() {
-  var thatWasEasy = new Audio("that_was_easy.mp3");
+  var thatWasEasy = new Audio("hioh.mp3");
   thatWasEasy.play();
 }
-
 $("#easy").on("click", sayThatWasEasy);
 
 $(document).keypress(delegateKeypress);
-
 function delegateKeypress(event) {
   if (event.charCode == 32) {
     $("#easy").trigger("click");
